8597679b7eff0834186545b6371bc5c8337a1343
Brad <brad@brad.tw>
2017-07-19 21:13:52 +0800

    2017071907

--- app/src/main/java/tw/brad/android/games/guessnumber/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/guessnumber/MainActivity.java
@@ -2,12 +2,25 @@ package tw.brad.android.games.guessnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button guess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        guess = (Button)findViewById(R.id.guess);
+        guess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("brad", "OK");
+            }
+        });
+
     }
 }
