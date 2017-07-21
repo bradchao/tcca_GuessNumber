df66d1d0a6d5314476262c24ce4a403fe2973ea2
Brad <brad@brad.tw>
2017-07-21 21:25:41 +0800

    2017072108

--- app/src/main/java/tw/brad/android/games/guessnumber/WelcomeActivity.java
+++ app/src/main/java/tw/brad/android/games/guessnumber/WelcomeActivity.java
@@ -20,6 +20,7 @@ public class WelcomeActivity extends AppCompatActivity {
                         new Intent(WelcomeActivity.this, MainActivity.class);
                 startActivity(it);
                 finish();
+                
             }
         });
 
