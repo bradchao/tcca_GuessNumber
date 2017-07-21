e463c4b04bbb981657a3aafd205640774d7610b0
Brad <brad@brad.tw>
2017-07-21 21:25:24 +0800

    2017072107

--- app/src/main/java/tw/brad/android/games/guessnumber/WelcomeActivity.java
+++ app/src/main/java/tw/brad/android/games/guessnumber/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package tw.brad.android.games.guessnumber;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        View view = findViewById(R.id.welcome);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it =
+                        new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+
     }
 }
