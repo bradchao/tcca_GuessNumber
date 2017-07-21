30309802d0c65c8ce4adb3695d87728d118eeffd
Brad <brad@brad.tw>
2017-07-21 19:44:15 +0800

    2017072101

--- app/src/main/java/tw/brad/android/games/guessnumber/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/guessnumber/MainActivity.java
@@ -5,27 +5,57 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private View guess;
+    private String answer;
+    private EditText input;
+    private TextView log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        input = (EditText)findViewById(R.id.input);
+        log = (TextView)findViewById(R.id.log);
+
+        answer = createAnswer();
+
         guess = findViewById(R.id.guess);
         guess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("brad", "OK2");
+                doGuess();
             }
         });
 
     }
 
+    private void doGuess(){
+        String strInput = input.getText().toString();
+
+        // 檢查幾A幾B
+        String result = checkAB();
+
+        log.append(strInput + " => " + result + "\n");
+
+        input.setText("");
+
+
+    }
+
     public void reset(View view){
 
+    }
+
+    private String createAnswer(){
+        return "175";
+    }
+    private String checkAB(){
+        return "1A2B";
     }
 
 }
